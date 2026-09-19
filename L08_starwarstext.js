1e10ccfe4ec579aa4c0b6d0211224b1aea7ca351
computhink-example <computhink-example@outlook.com>
2026-09-19 07:41:54 +0000

    1.09

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,1 +1,46 @@
-// write your codes here
+let yPos; // Text y pos
+let storyText = [
+    "A long time ago in a galaxy far,",
+    "far away...",
+    "", // Empty line
+    "EPISODE I",
+    "THE BEGINNING",
+    "", // Empty line
+    "It is a period of civil war.",
+    "Rebel spaceships, striking",
+    "from a hidden base, have",
+    "won their first victory",
+    "against the evil Galactic",
+    "Empire."
+];
+
+let bgMusic;
+
+function preload() {
+    bgMusic = loadSound("assets/star_wars_theme_8_bit.mp3");
+}
+
+function setup() {
+    createCanvas(600, 400);
+    textAlign(LEFT, CENTER);
+    textSize(24);
+    fill("rgb(255, 255, 0)");
+
+    // Set starting y pos
+    yPos = height;
+
+    // Start BGM
+    bgMusic.loop();
+}
+
+function draw() {
+    // Reset canvas / clear screen
+    background(0);
+
+    // Loop through array
+    for (let i = 0; i < storyText.length; i++) {
+        text(storyText[i], width / 4, yPos + i * 40); // (string, x pos, y pos)
+    }
+    // Decrease y to move up
+    yPos -= 0.5;
+}
--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,1 +1,26 @@
-// write your codes here
+let hr;
+let min;
+let sec;
+let timeString;
+
+function setup() {
+    createCanvas(600, 400);
+    textAlign(CENTER, CENTER);
+    textSize(40);
+    fill("rgb(255, 255, 50)");
+}
+
+function draw() {
+    // Reset canvas / clear screen
+    background(0);
+
+    // Set to current time
+    hr = hour();    // Returns current hour
+    min = minute(); // Returns current minute
+    sec = second(); // Returns current second
+
+    // nf = number format, nf(number, digits)
+    timeString = nf(hr, 2) + " : " + nf(min, 2) + " : " + nf(sec, 2);
+
+    text(timeString, width / 2, height / 2);
+}
