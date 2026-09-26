e42e1c29fe9096a51de3e30a551eb6aecfe10d26
computhink-example <computhink-example@outlook.com>
2026-09-26 07:37:04 +0000

    1.10

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,26 +1,87 @@
-let hr;
-let min;
-let sec;
-let timeString;
+// let hr;
+// let min;
+// let sec;
+// let timeString;
+
+// let intervalId;
+
+// function setup() {
+//     createCanvas(600, 400);
+//     // textAlign(CENTER, CENTER);
+//     // textSize(40);
+//     // fill("rgb(255, 255, 50)");
+// }
+
+// function draw() {
+//     // Reset canvas / clear screen
+//     background(0);
+
+//     // Set to current time
+//     hr = hour();    // Returns current hour
+//     min = minute(); // Returns current minute
+//     sec = second(); // Returns current second
+
+//     // nf = number format, nf(number, digits)
+//     timeString = nf(hr, 2) + " : " + nf(min, 2) + " : " + nf(sec, 2);
+
+//     text(timeString, width / 2, height / 2);
+// }
+
+// let x = 0;
+
+let intervalId; // Used to stop the interval
+
+let countdown = 5;
+let countdownStart = false;
 
 function setup() {
     createCanvas(600, 400);
-    textAlign(CENTER, CENTER);
+
+    // Set up text settings
+    fill(255, 0, 0);
     textSize(40);
-    fill("rgb(255, 255, 50)");
+
+    // Creates a loop to call drawCircle every 1000ms (1 second)
+    // intervalId = setInterval(drawCircle, 1000);
 }
 
 function draw() {
     // Reset canvas / clear screen
     background(0);
 
-    // Set to current time
-    hr = hour();    // Returns current hour
-    min = minute(); // Returns current minute
-    sec = second(); // Returns current second
+    text(countdown, width / 2, height / 2);
+}
 
-    // nf = number format, nf(number, digits)
-    timeString = nf(hr, 2) + " : " + nf(min, 2) + " : " + nf(sec, 2);
+function drawCircle() {
+    clear();
+    circle(x, 50, 20); // circle(x pos, y pos, diameter)
+    x += 10; // Move circle right
+    if (x > 200) {
+        x = 0;
+    }
+}
 
-    text(timeString, width / 2, height / 2);
+// This function is called whenever you click your mouse
+function mousePressed() {
+    // Stop an interval using its ID
+    // clearInterval(intervalId);
+
+    if (countdownStart === false) {
+        // Prevent multiple intervals
+        countdownStart = true;
+        countdown = 5;
+        intervalId = setInterval(updateCountdown, 1000);
+    }
+}
+
+function updateCountdown() {
+    countdown--;
+
+    // Stop interval after countdown reaches 0
+    if (countdown <= 0) {
+        countdown = 0;
+        clearInterval(intervalId);
+        // Allow timer to restart
+        countdownStart = false;
+    }
 }
--- L10_name.js
+++ L10_name.js
@@ -1,1 +1,33 @@
-// write your codes here
+let inputText; // Stores user input
+let displayText = "Your Name Here"; // Text to display on canvas
+
+function setup() {
+    createCanvas(600, 400);
+
+    // Set text settings
+    fill(255, 255, 0);
+    textSize(40);
+    textAlign(CENTER, CENTER);
+
+    // Create input field
+    inputText = createInput();
+    // Shift input field into canvas
+    let inputX = this.canvas.offsetLeft + (width / 2) - 80;
+    let inputY = this.canvas.offsetTop + (height / 2) - 10;
+    inputText.position(inputX, inputY);
+
+    // Call updateText when user types
+    inputText.input(updateText);
+}
+
+function draw() {
+    background(0);
+
+    text(displayText, width / 2, height * 0.3);
+}
+
+function updateText() {
+    // Save input to displayText whenever user types
+    displayText = this.value();
+    console.log(displayText);
+}
